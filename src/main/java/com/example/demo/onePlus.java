84f97ccc224188a6d6363e41726acb13d78f648f
Eswar Arjun <eswararjun7981@gmail.com>
2026-03-16 10:37:38 +0530

    Onplus.java is Updated

--- src/main/java/com/example/demo/onePlus.java
+++ src/main/java/com/example/demo/onePlus.java
@@ -11,5 +11,6 @@ public class onePlus implements Mobiles{
 	}
 	public static void main(String[] args) {
 		System.out.println("Hello world");
+		System.out.println("My Name is Singamsetty Eswar Arjun");
 	}
 }
